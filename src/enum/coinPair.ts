70af617dccf28cb0514e2723a339f05df563e945
wallace <wallace@iMac-de-wallace.local>
2019-10-11 20:35:27 -0400

    adicionar coinpair no enum

---
+++ src/enum/coinPair.ts
@@ -0,0 +1,11 @@
+export const validsCoinPair = [
+  'BRLBTC'
+]
+
+export default {
+  'BRLBTC': 'Real e Bitcoin',
+  'BRLBCH': 'Real e BCash',
+  'BRLETH': 'Real e Ethereum',
+  'BRLLTC': 'Real e Litecoin',
+  'BRLXRP': 'Real e Litecoin'
+};
