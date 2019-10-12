1c00150a8901b0ec91f0dd3bdc5ecea490ebf881
wallace <wallace@iMac-de-wallace.local>
2019-10-11 20:33:28 -0400

    ajusta env e insere novas chaves necessárias

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,7 +11,11 @@ const envVarsSchema: JoiObject = Joi.object({
     .default('production')
     .required(),
   PORT: Joi.number().required().default(8000).description('Porta usada pela API'),
-  BASE_URL: Joi.string().required()
+  BASE_URL: Joi.string().required().description('Url base do mercado bitcoin'),
+  KEY: Joi.string().length(32).required().description('Key da API mercado bitcoin'),
+  SECRET: Joi.string().required().description('SECRET da API mercado bitcoin'),
+  API_PATH: Joi.string().required().description('PATH API mercado bitcoin'),
+  TAPI_PATH: Joi.string().required().description('PATH TAPI mercado bitcoin')
 }).unknown().required();
 
 const { error, value: envVars }: { error: ValidationError, value: any } = Joi.validate(process.env, envVarsSchema);
@@ -32,8 +36,12 @@ const config = {
   PORT: envVars.PORT,
   BASE_URL: envVars.BASE_URL,
   LOG_TYPE: getLogType(),
-  API_PATH: envVars.BASE_URL + 'api/',
-  TAPI_PATH: envVars.BASE_URL + 'tap/'
+  KEY: envVars.KEY,
+  SECRET: envVars.SECRET,
+  API_PATH: envVars.API_PATH,
+  TAPI_PATH: envVars.TAPI_PATH,
+  API_URL: envVars.BASE_URL + envVars.API_PATH,
+  TAPI_URL: envVars.BASE_URL + envVars.TAPI_PATH,
 };
 
 export default config;
